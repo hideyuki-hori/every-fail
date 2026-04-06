d8812c7d466e6fba03171d28dacb14922407ed0f
- <montecarlo1101@gmail.com>
2026-04-07 08:43:24 +0900

    cors対応

--- apps/site/src/main.ts
+++ apps/site/src/main.ts
@@ -2,3 +2,5 @@ import { Effect } from 'effect'
 import './styles/index.css'
 
 Effect.runSync(Effect.sync(() => console.log(1)))
+
+fetch('http://localhost:8788/api/dots/a').then(a => a.text()).then(console.log)
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -4,22 +4,38 @@ interface Env {
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, OPTIONS',
+          'Access-Control-Allow-Headers': '*',
+        },
+      })
+    }
+
     const url = new URL(request.url)
     const path = url.pathname
+
+    const corsHeaders = {
+      'Access-Control-Allow-Origin': '*',
+    }
 
     const dotsMatch = path.match(/^\/api\/dots\/([^/]+)$/)
     if (dotsMatch) {
       const name = dotsMatch[1]
-      return env.ASSETS.fetch(new URL(`/${name}/index.html`, url.origin))
+      const res = await env.ASSETS.fetch(new URL(`/${name}/index.html`, url.origin))
+      return new Response(res.body, { ...res, headers: { ...Object.fromEntries(res.headers), ...corsHeaders } })
     }
 
     const assetMatch = path.match(/^\/api\/dots\/([^/]+)\/(.+)$/)
     if (assetMatch) {
       const name = assetMatch[1]
       const file = assetMatch[2]
-      return env.ASSETS.fetch(new URL(`/${name}/${file}`, url.origin))
+      const res = await env.ASSETS.fetch(new URL(`/${name}/${file}`, url.origin))
+      return new Response(res.body, { ...res, headers: { ...Object.fromEntries(res.headers), ...corsHeaders } })
     }
 
-    return new Response('Not Found', { status: 404 })
+    return new Response('Not Found', { status: 404, headers: corsHeaders })
   },
 }
