ebfc12ccfd4ac88c7b50dd083277432084451cbf
- <montecarlo1101@gmail.com>
2026-04-08 07:03:40 +0900

    HttpClient追加

--- apps/site/src/main.ts
+++ apps/site/src/main.ts
@@ -1,6 +1,17 @@
-import { Effect } from 'effect'
+import { Console, Effect, pipe } from 'effect'
+import { FetchHttpClient, HttpClient } from '@effect/platform'
 import './styles/index.css'
 
-Effect.runSync(Effect.sync(() => console.log(1)))
+const fetchDotHtml = (id: string) => Effect.gen(function* () {
+  const client = yield* HttpClient.HttpClient
+  const res = yield* client.get(`http://localhost:8788/api/dots/${id}`)
+  return yield* res.text
+})
 
-fetch('http://localhost:8788/api/dots/a').then(a => a.text()).then(console.log)
+const rootEffect = pipe(
+  fetchDotHtml('a'),
+  Effect.tap(Console.log),
+  Effect.provide(FetchHttpClient.layer),
+)
+
+Effect.runPromise(rootEffect)
